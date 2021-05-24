00b90b58d9c7127b303e697263d80567ba3305ca
Seiji Nakaya <snakaya@loosedays.jp>
2021-05-25 07:15:23 +0900

    use generated tmpdir when statefile not abs path

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,21 @@ import { pollingAsync } from "./pollingJma";
 import "./utils/env";
 import { CronJob } from "cron";
 import config from "config";
-
+import os from 'os';
+import path from 'path';
 
 const settingsInfo: any = config.get("Settings");
 const slackInfo: any = config.get("Slack");
 const weatherURL: string = settingsInfo.WeatherURL;
 const eqvolURL: string = settingsInfo.EqvolURL;
-const stateWeatherCacheFile: string = settingsInfo.StateWeatherFileName;
-const stateEvolCacheFile: string = settingsInfo.StateEvolFileName;
+console.log(path.dirname(settingsInfo.StateWeatherFileName as string));
+const stateWeatherCacheFile: string = (path.dirname(settingsInfo.StateWeatherFileName as string) == '.') ? os.tmpdir() + path.sep + settingsInfo.StateWeatherFileName : settingsInfo.StateWeatherFileName;
+const stateEvolCacheFile: string = (path.dirname(settingsInfo.StateEvolFileName as string) == '.') ? os.tmpdir() + path.sep + settingsInfo.StateEvolFileName : settingsInfo.StateEvolFileName;
 
 console.log('Slack notify: ' + slackInfo.notify.webhook + ' at ' + slackInfo.notify.channel);
 console.log('Slack error : ' + slackInfo.error.webhook + ' at ' + slackInfo.error.channel);
-console.log('state Weather file : ' + settingsInfo.StateWeatherFileName);
-console.log('state Evol file    : ' + settingsInfo.StateEvolFileName);
+console.log('state Weather file : ' + stateWeatherCacheFile);
+console.log('state Evol file    : ' + stateEvolCacheFile);
 
 const evolCronJob = new CronJob('*/1 * * * * *', async () =>{await pollingAsync(eqvolURL, stateEvolCacheFile)});
 if (!evolCronJob.running) {
